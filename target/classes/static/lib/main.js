be48e0140ae035b857b48d0497881c0d349d7ac9
Ashrujit Pal <ashrujitpal@gmail.com>
2020-04-10 17:42:04 +0400

    first commit

--- target/classes/static/lib/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-$('.request-block__container').each(function( index, element ) {
-  $(element).click(function(){
-    $(element).toggleClass("request-block__container--selected");
-  })
-});
